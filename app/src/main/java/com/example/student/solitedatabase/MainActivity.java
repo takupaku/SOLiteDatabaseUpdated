8a028eaa22bc25698ae6cc8baafa6d912cdaddb5
taku paku <takupaku@yahoo.com>
2018-09-23 14:13:35 +0600

    update, delete, search(error existes,check sql query)

--- app/src/main/java/com/example/student/solitedatabase/MainActivity.java
+++ app/src/main/java/com/example/student/solitedatabase/MainActivity.java
@@ -10,8 +10,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-   private EditText etId, etName,etArea;
-   private TextView result;
+    private EditText etId, etName, etArea, etNumber;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,40 +22,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        etId=findViewById(R.id.etId);
-        etName =findViewById(R.id.etName);
-        etArea=findViewById(R.id.etArea);
-        result=findViewById(R.id.result);
+        etId = findViewById(R.id.etId);
+        etName = findViewById(R.id.etName);
+        etArea = findViewById(R.id.etArea);
+        etNumber = findViewById(R.id.etNumber);
+        result = findViewById(R.id.result);
 
     }
 
     public void save(View view) {
         String id = etId.getText().toString().trim();
-        String name= etName.getText().toString().trim();
-        String area= etArea.getText().toString().trim();
-        if (id.isEmpty()){
+        String name = etName.getText().toString().trim();
+        String area = etArea.getText().toString().trim();
+        String number = etNumber.getText().toString().trim();
+        if (id.isEmpty()) {
             etId.setError("please enter id");
             etId.requestFocus();
             return;
         }
 
-        if(name.isEmpty()){
+        if (name.isEmpty()) {
             etName.setError("please enter id");
             etName.requestFocus();
             return;
         }
 
-        if(area.isEmpty()){
+        if (area.isEmpty()) {
             etArea.setError("please enter id");
             etArea.requestFocus();
             return;
         }
 
-        MyDBClass myDBClass = new MyDBClass(this);
-        if(myDBClass.insertData(id, name, area)){
-            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+        if (number.isEmpty()) {
+            etNumber.setError("please enter id");
+            etNumber.requestFocus();
+            return;
         }
-        else Toast.makeText(this, "Something Wrong", Toast.LENGTH_SHORT).show();
+
+        MyDBClass myDBClass = new MyDBClass(this);
+        if (myDBClass.insertData(id, name, area)) {
+            Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
+        } else Toast.makeText(this, "Something Wrong", Toast.LENGTH_SHORT).show();
 
     }
 
@@ -63,21 +70,92 @@ public class MainActivity extends AppCompatActivity {
         MyDBClass myDBClass = new MyDBClass(this);
         Cursor cursor = myDBClass.viewData();
 
-        StringBuilder builder= new StringBuilder();
-        if(cursor== null){
+        StringBuilder builder = new StringBuilder();
+        if (cursor == null) {
             result.setText("no data found");
             return;
         }
-        cursor.moveToFirst();
 
-        do{
-            builder.append("Id : ").append(cursor.getString(0))
-                    .append(" , Name: ").append(cursor.getString(1)).append(" . ")
-                    .append("Area : ").append(cursor.getString(2)).append("\n\n");
+        if (cursor.getCount() > 0) {
+            cursor.moveToFirst();
+            for (int i = 0; i < cursor.getCount(); i++) {
+                builder.append("Id : ").append(cursor.getString(0))
+                        .append(" , Name: ").append(cursor.getString(1)).append(" . ")
+                        .append("Area : ").append(cursor.getString(2)).append("Number : ").append(cursor.getString(3)).append("\n\n");
+                cursor.moveToNext();
+
+            }
+
+            result.setText(builder);
+        } else {
+            result.setText("no data found");
+        }
 
 
-        }while (cursor.moveToNext());
-        result.setText(builder);
+    }
+
+    public void updateData(View view) {
+        String id = etId.getText().toString().trim();
+        String name = etName.getText().toString().trim();
+        String area = etArea.getText().toString().trim();
+        String number = etNumber.getText().toString().trim();
+
+        MyDBClass myDBClass = new MyDBClass(this);
+        if(myDBClass.updateData(id, name, area, number) == -1){
+            Toast.makeText(this, "failed to updated", Toast.LENGTH_SHORT).show();
+
+        }
+        else{
+            Toast.makeText(this, "updated", Toast.LENGTH_SHORT).show();
+            emptyText();
+        }
+    }
+
+    private void emptyText() {
+        etId.setText("");
+        etNumber.setText("");
+        etArea.setText("");
+        etName.setText("");
+
+    }
+
+    public void Delete(View view) {
+        String id = etId.getText().toString().trim();
+
+        if(id.isEmpty()){
+            etId.setError("enter id");
+            etId.requestFocus();
+            return;
+        }
+        MyDBClass myDBClass = new MyDBClass(this);
+        if(myDBClass.deleteData(id)== -1){
+            Toast.makeText(this, "failed to delete", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Toast.makeText(this, "updated", Toast.LENGTH_SHORT).show();
+            emptyText();
+        }
+    }
+
+    public void search(View view) {
+        String id = etId.getText().toString().trim();
+        if(id.isEmpty()){
+            etId.setError("enter id");
+            etId.requestFocus();
+            return;
+        }
+        MyDBClass myDBClass = new MyDBClass(this);
+        Cursor cursor = myDBClass.searchItem(id);
+        if(cursor.getCount() >0 ){
+            cursor.moveToFirst();
+            etName.setText(cursor.getString(1));
+            etArea.setText(cursor.getString(2));
+            etNumber.setText(cursor.getString(3));
+
+        }
+        else{
+            Toast.makeText(this, "not found", Toast.LENGTH_SHORT).show();
+        }
 
     }
 }
--- app/src/main/java/com/example/student/solitedatabase/MyDBClass.java
+++ app/src/main/java/com/example/student/solitedatabase/MyDBClass.java
@@ -5,22 +5,25 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
 
 public class MyDBClass extends SQLiteOpenHelper {
     private static final String TABLE_NAME = "my_table";
     private static final String COL_1 = "id";
     private static final String COL_2 = "name";
     private static final String COL_3 = "area";
+    private Context context;
 
     public MyDBClass(Context context) {//changing version will change(update,insert bla bla..) the table
         super(context, "my_db.db", null, 1);
+        this.context = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
 
-        String sql= "create table " + TABLE_NAME + " "+
-                "( "+ COL_1 +" integer primary key , "+ COL_2 +" text , "+ COL_3 + "text )";
+        String sql = "create table " + TABLE_NAME +
+                "(" + COL_1 + " integer primary key, " + COL_2 + " text, " + COL_3 + "text)";
 
         sqLiteDatabase.execSQL(sql);
 
@@ -28,14 +31,22 @@ public class MyDBClass extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        try {
+            String sql = "alter table " + TABLE_NAME + " add column number text";
+            sqLiteDatabase.execSQL(sql);
+            Toast.makeText(context, "table updated", Toast.LENGTH_SHORT).show();
 
-        String sql = "drop table if exists " + TABLE_NAME;
-        sqLiteDatabase.execSQL(sql);
-        onCreate(sqLiteDatabase);
+
+        } catch (Exception e) {
+            Toast.makeText(context, "table not updated ", Toast.LENGTH_SHORT).show();
+        }
+//        String sql = "drop table if exists " + TABLE_NAME;
+//        sqLiteDatabase.execSQL(sql);
+//        onCreate(sqLiteDatabase);
 
     }
 
-    public  boolean  insertData(String id,String name,String area){
+    public boolean insertData(String id, String name, String area) {
         SQLiteDatabase database = getWritableDatabase();
         ContentValues values = new ContentValues();
 
@@ -43,21 +54,47 @@ public class MyDBClass extends SQLiteOpenHelper {
         values.put(COL_2, name);
         values.put(COL_3, area);
 
-       long result=  database.insert(TABLE_NAME, null, values);
-       if(result == -1)
-           return false;
-       else
-           return true;
+        long result = database.insert(TABLE_NAME, null, values);
+        if (result == -1)
+            return false;
+        else
+            return true;
 
 
+    }
 
-        }
-        public Cursor viewData(){
+    public Cursor viewData() {
         SQLiteDatabase database = getReadableDatabase();
-        String sql= "select * from " + TABLE_NAME;
-        Cursor cursor= database.rawQuery(sql, null);
+        String sql = "select * from " + TABLE_NAME;
+        Cursor cursor = database.rawQuery(sql, null);
         return cursor;
-        }
+    }
+
+    public int deleteData(String id) {
+        SQLiteDatabase database = getWritableDatabase();
+        //return database.delete(TABLE_NAME, "where id = ?",new String[]{id});
+        return database.delete(TABLE_NAME, " = ? " + COL_1, new String[]{id});
+    }
+
+    public int updateData(String id, String name, String area, String number) {
+        SQLiteDatabase database = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COL_2, name);
+        values.put(COL_3, area);
+        values.put(number, number);
+
+        return database.update(TABLE_NAME, values, " = ? " + COL_1, new String[]{id});
+
+
+    }
+
+
+    public Cursor searchItem(String id) {
+        SQLiteDatabase database = getReadableDatabase();
+        String sql= "select * from "+ TABLE_NAME+ " where id = "+ id;
+        return  database.rawQuery(sql, null);
+
+    }
 
 
 }
